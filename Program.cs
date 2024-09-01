a8da21021d96821c9c0c1eb4e46c028bcb2a24ab
Filipe Husak <69702727+flp04@users.noreply.github.com>
2024-09-01 16:13:02 -0300

    realizado soluções solicitadas

--- Program.cs
+++ Program.cs
@@ -1,3 +1,19 @@
-﻿using DesafioPOO.Models;
+﻿using System;
+using DesafioPOO.Models;
+
+class Program
+{
+  static void Main(string[] args)
+  {
+    Nokia aparelho1 = new("11995875266", "Azul", "123456789", 8);
+    Iphone aparelho2 = new("11958234678", "Preto", "987654321", 4);
+    aparelho1.Ligar();
+    aparelho2.ReceberLigacao();
+    aparelho2.Ligar();
+    aparelho1.ReceberLigacao();
+    aparelho1.InstalarAplicativo("WhatsApp");
+    aparelho2.InstalarAplicativo("WhatsApp");
+  }
+}
 
 // TODO: Realizar os testes com as classes Nokia e Iphone
